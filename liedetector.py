24a04f5d2735b4b6107baa360e87c2cee3989c34
kz6fittycent <jimmy.tigert@gmail.com>
2018-07-10 19:59:26 -0500

    Update liedetector.py
    just more stupidity...

--- liedetector.py
+++ liedetector.py
@@ -10,7 +10,7 @@ print ("             WELCOME TO LIE DETECTOR")
 print (50 * '-')
 print (50 * '#')
 time.sleep(1)
-print ("MIT License, v. 1.0")
+print ("MIT License, v. 1.1")
 time.sleep(2)
 print()
 
@@ -23,10 +23,19 @@ def main():
     if choice.startswith('y'):
        print()
        time.sleep(2.0)
+       print("That's interesting...")
+       time.sleep(1.5)
+       print()
        print("LIAR!!!")
       
     else:
        choice.startswith('n')
+       print()
+       time.sleep(2.0)
+       print("hmmmmmm....")
+       print()
+       time.sleep(1.0)
+       print("hmmm...")
        print()
        time.sleep(2.0)
        print("LYING LIAR PANTS!!!")
